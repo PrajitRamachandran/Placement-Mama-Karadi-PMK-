5c3cf547c701af439f0c8f840c2d8e2f3ffbae84
PrajitRamachandran <137390813+PrajitRamachandran@users.noreply.github.com>
2025-02-11 13:45:59 +0530

    Add files via upload

---
+++ butterfly.py
@@ -0,0 +1,5 @@
+def butterfly_pattern(n):
+    for i in range(1, n + 1):
+        print("* " * i + "  " * (n - i) * 2 + "* " * i)
+    for i in range(n - 1, 0, -1):
+        print("* " * i + "  " * (n - i) * 2 + "* " * i)
